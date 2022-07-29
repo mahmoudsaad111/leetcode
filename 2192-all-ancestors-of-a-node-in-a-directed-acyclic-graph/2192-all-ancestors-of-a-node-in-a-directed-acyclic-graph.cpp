3b28605d9433305dd26b8f828117288496641d9b
mahmoudsaad111 <88971364+mahmoudsaad111@users.noreply.github.com>
2022-07-30 01:22:55 +0200

    Time: 228 ms (86.58%), Space: 132.5 MB (53.81%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>&g,vector<int>&visted,vector<vector<int>>&ans,int i,int p){
+        for(int  j=0;j<g[i].size();j++){
+            
+            if(!visted[g[i][j]]){
+                visted[g[i][j]]=1;
+                dfs(g,visted,ans,g[i][j],p) ;
+                ans[g[i][j]].push_back(p) ;
+            }
+        }
+        
+    }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>>g(n) ;vector<int>d(n);
+        vector<vector<int>>g(n) ;
         for(int i=0;i<edges.size();i++){
             g[edges[i][0]].push_back(edges[i][1]) ;
-            d[edges[i][1]]++ ;
         }
-        vector<set<int>>vec(n) ;
-        queue<pair<int,set<int>>>q ; 
+       vector<vector<int>>ans(n) ;
         for(int i=0;i<n;i++){
-            if(!d[i]){
-                set<int>st;st.insert(i) ;
-                q.push({i,st}) ;
-            }
+            vector<int>visted(n) ;
+            visted[i]=1;
+            dfs(g,visted,ans,i,i); 
         }
-  
-            while(q.size()){
-                int x=q.front().first;set<int>st=q.front().second ;q.pop() ;
-                
-                for(int i=0;i<g[x].size();i++){
-                    for(auto j=st.begin();j!=st.end();j++)vec[g[x][i]].insert(*j) ;
-                    vec[g[x][i]].insert(g[x][i]) ;
-                    if(--d[g[x][i]]==0)q.push({g[x][i],vec[g[x][i]]}) ;
-                }
-            }
-        vector<vector<int>>ans(n) ;
-        for(int i=0;i<n;i++){
-            for(auto j=vec[i].begin();j!=vec[i].end();j++){
-                if(i!=*j)
-                ans[i].push_back(*j) ;
-            }
-        }
+      
+     
         return ans; 
         
         
