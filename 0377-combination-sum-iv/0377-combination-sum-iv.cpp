9b9bf2e05616c204dd0185f32db1c8032fbf9a6a
mahmoudsaad111 <88971364+mahmoudsaad111@users.noreply.github.com>
2022-10-14 17:27:00 +0200

    Time: 7 ms (21.76%), Space: 6.6 MB (92.99%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int arr[1001][201]; 
-    int dp(int ind,int m,vector<int>&vec){
+    int arr[1001]; 
+    int dp(int m,vector<int>&vec){
         if(!m)return 1; 
-        if(ind>=vec.size())return 0 ;
-        int &ret=arr[m][ind]; 
+        int &ret=arr[m]; 
         if(ret!=-1)return ret; 
         ret=0 ;
        for(int i=0;i<vec.size();i++){
            if(vec[i]<=m){
-               ret+=dp(i,m-vec[i],vec) ; 
+               ret+=dp(m-vec[i],vec) ; 
            }
        }
         return ret; 
     }
     int combinationSum4(vector<int>& nums, int target) {
         memset(arr,-1,sizeof(arr)) ;
-        return dp(0,target,nums); 
+        return dp(target,nums); 
     }
 };
